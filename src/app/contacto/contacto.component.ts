421ab56e299d395a6872b2ae0b839193d239cba3
BarbaraSilvaJ <al244923@edu.uaa.mx>
2024-05-06 16:39:44 -0600

    ruta

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
 import { FooterComponent } from "../footer/footer.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-contacto',
     standalone: true,
     templateUrl: './contacto.component.html',
     styleUrl: './contacto.component.css',
-    imports: [MenuComponent, FooterComponent]
+    imports: [MenuComponent, FooterComponent, RouterModule]
 })
 export class ContactoComponent {
 
